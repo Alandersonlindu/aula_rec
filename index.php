8b54f54f2a707a78c5a3f114aaf6cddaced23c24
Alandersonlindu <afonso3966@gmail.com>
2023-06-29 08:11:26 -0300

    Aula de recuperação

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$num = 11;
+$num2 = 50;
+$soma = $num + $num2;
+if($num > 60){
+    echo "é maior que 50 ";
+}
+else{
+    echo "é menor que 60"
+}
+?>
---
+++ web.php
@@ -0,0 +1,5 @@
+<?php
+for ($i = 1; $i<= 50; $i++) {
+    echo "Eu vou passar em web!!<br>";
+}
+?>
